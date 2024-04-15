ca8f3a5f2eb81d74316ed5fea26627ada1bd4346
abdul <abdulanm@gmail.com>
2024-04-15 16:03:59 +0530

    Add project files.

---
+++ Abdul39AA/Migrations/20240313190654_CodeFirstAdded23.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abdul39AA.Migrations
+{
+    /// <inheritdoc />
+    public partial class CodeFirstAdded23 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
